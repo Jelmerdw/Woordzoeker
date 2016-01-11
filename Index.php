db99d93674af49f2366a27146b5d8edbdb1b399b
Jelmer de Wolde <147825@helenpark.nl>
2016-01-11 16:27:44 +0100

    Begin

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<title>Woordzoeker</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<p>Hoi dit is ons prachtige project.</p>
+
+<?php
+echo file_get_contents("text.txt");
+?>
+
+<br>
+
+ <?php
+  $data = fopen("text.txt", "r");
+  echo $data ;
+ ?>
+
+</body>
