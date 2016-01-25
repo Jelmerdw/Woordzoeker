47d2a15ffd4c77076c5447b9dd00690a52ae6970
Luna Hammecher <148573@hpleerling.nl>
2016-01-25 16:24:55 +0100

    g

--- test.php
+++ test.php
@@ -2,18 +2,24 @@
 
 $letters = "abcdef...z";
 $regel = array('-', 'W');
-foreach($regel as $i => $W) {
+$regel = str_split("-Wat----");
+foreach ($regel as $i => $W) {
     if ($W == '-') {
-  $l = random(0,26);
-          $regel[$i] = $letters[$l];
-    }  
+        $l = rand(0, 3);
+       // print_r($regel);
+        $regel[$i] = $letters[$l];
+        //print_r($regel);
+    }
 }
-$regels = FILE("bestand");
-foreach($regels as $regelNr => $data) {
-    
-    if ($data == '');{ 
+print implode("", $regel);
+//print_r($regel);
+$regels = FILE("text.txt");
+foreach ($regels as $regelNr => $data) {
+
+    if ($data == '') {
         break;
     }
     $wz[$regelNr] = $data;
 }
+//print_r($wz);
 ?>
