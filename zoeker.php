02680e3f6e8677d05b90effa7804c6dc33121bc4
Jelmer de Wolde <147825@hpleerling.nl>
2016-01-29 12:15:21 +0100

    Dingen in functies gezet, werkt wel maar krijg wel meldingen waarvan ik niet snap waarom

--- zoeker.php
+++ zoeker.php
@@ -1,22 +1,22 @@
 <?php
+
 // Start the session
 session_start();
 ?>
 <?php
+
 $woorden = $_SESSION["woorden"];
 $keuze = $_GET['keuze'];
 
 if ($keuze == 3) {
     $keuze = 3;
-} 
-else {
+} else {
     if ($keuze == 2) {
         $keuze = 2;
     } else {
         $keuze = 1;
     }
 }
-
 
 $zoek = $woorden[$keuze];
 $herhalingen = strlen($zoek);
@@ -30,58 +30,88 @@ $kolom = 0;
 $regel = 0;
 $einde = 0;
 
+function gevonden($regel, $aantal_kolommen, $kolom) {
+    global $regel, $aantal_kolommen, $kolom;
+    
+    $cel = $regel * $aantal_kolommen + $kolom;
+    kleurCel($cel, 'yellow');
+    $kolom = $kolom + 1;
+    uitvoeringen($uitgevoerd, $herhalingen, $einde);
+}
+
+function niet_gevonden($kolom, $uitgevoerd, $cel) {
+    global $kolom, $uitgevoerd, $cel;
+    
+    $kolom = $kolom + 1;
+    $uitgevoerd = 0;
+    $cel = 0;
+    herstel($cel, $aantal_klommen, $aantal_regels);
+}
+
+function uitvoeringen($uitgevoerd, $herhalingen, $einde) {
+    global $uitgevoerd, $herhalingen, $einde;
+    
+    if ($uitgevoerd < $herhalingen) {
+        $uitgevoerd = $uitgevoerd + 1;
+    }
+    if ($uitgevoerd == $herhalingen) {
+        $einde = 1;
+    }
+}
+
+function herstel($cel, $aantal_klommen, $aantal_regels) {
+    global $cel, $aantal_kolommen, $aantal_regels;
+    
+    while ($cel < $aantal_kolommen * $aantal_regels) {
+        kleurCel($cel, 'white');
+        $cel = $cel + 1;
+    }
+}
+
 function kleurCel($cel, $kleur) {
     echo '<script type="text/javascript">';
     echo ' $(".cel' . $cel . '").css("background-color", "' . $kleur . '");';
     echo '</script>';
 }
 
-//print "<pre>";print_r($_SESSION); print "</pre>";
-print "herhalingen $herhalingen";
+//print "<pre>";print_r($_SESSION); 
+//print "</pre>";
+//print "herhalingen $herhalingen";
 //exit;
+
 $teller = 0;
-while ($uitgevoerd < $herhalingen || $teller < 10) {
-    $teller++;
+while ($uitgevoerd < $herhalingen) {
     $regel = 0;
-    //unset($array_cel);
     while ($regel < $aantal_regels) {
         $kolom = 0;
         while ($kolom < $aantal_kolommen and $einde == 0) {
             if ($woordzoeker[$regel][$kolom] == $arrayzoek[$uitgevoerd]) {
-                //echo $regel;
-                //echo $kolom;
-                $cel = $regel * $aantal_kolommen + $kolom;
-                // $array_cel[] = $cel;
-                kleurCel($cel, 'yellow');
-                //<script type="text/javascript">
-                //    $(".cel<?php echo $cel; ? >").css("background-color", "yellow");
-                //</ script>
-                $kolom = $kolom + 1;
-                if ($uitgevoerd < $herhalingen) {
-                    $uitgevoerd = $uitgevoerd + 1;
-                }
-                if ($uitgevoerd == $herhalingen) {
-                    $einde = 1;
-                    break;  // leave inner while loop
-                }
+                gevonden($regel, $aantal_kolommen, $kolom);
+                //$cel = $regel * $aantal_kolommen + $kolom;
+                //kleurCel($cel, 'yellow');
+                //$kolom = $kolom + 1;
+                //uitvoeringen($uitgevoerd, $herhalingen, $einde);
+                //if ($uitgevoerd < $herhalingen) {
+                //$uitgevoerd = $uitgevoerd + 1;
+                //}
+                //if ($uitgevoerd == $herhalingen) {
+                //$einde = 1;
+                //}
             } else {
-                $kolom = $kolom + 1;
-                $uitgevoerd = 0;
-                $cel = 0;
-                while ($cel < $aantal_kolommen * $aantal_regels) {
-                    ?>
-                    <script type="text/javascript">
-                        $(".cel<?php echo $cel; ?>").css("background-color", "white");
-                    </script>
-                    <?php
-                    $cel = $cel + 1;
-                }
+                niet_gevonden($kolom, $uitgevoerd, $cel);
+                ///$kolom = $kolom + 1;
+                //$uitgevoerd = 0;
+                //$cel = 0;
+                //herstel($cel, $aantal_klommen, $aantal_regels);
+                //while ($cel < $aantal_kolommen * $aantal_regels) {
+                //kleurCel($cel, 'white');
+                //$cel = $cel + 1;
+                //}
             }
         }
         $regel = $regel + 1;
     }
     $uitgevoerd = $uitgevoerd + 1;
-    //$letter[] = $array_cel;
 }
 
 echo $keuze;
