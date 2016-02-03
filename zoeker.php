2e99c6e0ef7ffed46f56add56fe58167ffc99d7e
Jelmer <147825@helenparkhurst.net>
2016-02-03 11:20:33 +0100

    Werkt iets vloeiender, paar bugs weg, gebruiker weet wanneer de zoeker bezig is door loading-teken.

--- zoeker.php
+++ zoeker.php
@@ -1,22 +1,8 @@
 <?php
-
-// Start the session
 session_start();
-?>
-<?php
 
 $woorden = $_SESSION["woorden"];
 $keuze = $_GET['keuze'];
-
-//if ($keuze == 3) {
-    //$keuze = 3;
-//} else {
-    //if ($keuze == 2) {
-        //$keuze = 2;
-    //} else {
-        //$keuze = 1;
-    //}
-//}
 
 $zoek = $woorden[$keuze];
 $herhalingen = strlen($zoek);
@@ -29,7 +15,6 @@ $uitgevoerd = 0;
 $kolom = 0;
 $regel = 0;
 $einde = 0;
-$vorige_cel = -1;
 $keer = 0;
 
 function regelplaats(&$regel, &$aantal_regels, &$kolom) {
@@ -43,9 +28,7 @@ function regelplaats(&$regel, &$aantal_regels, &$kolom) {
 function zoek(&$kolom, &$aantal_kolommen, &$einde, &$woordzoeker, &$regel, &$arrayzoek, &$uitgevoerd, &$cel) {
     while ($kolom < $aantal_kolommen and $einde == 0) {
         if ($woordzoeker[$regel][$kolom] == $arrayzoek[$uitgevoerd]) {
-            //if($vorige_cel == -1){
             gevonden($regel, $aantal_kolommen, $kolom);
-            //}
         } else {
             niet_gevonden($kolom, $uitgevoerd, $cel);
         }
@@ -104,39 +87,32 @@ while ($uitgevoerd < $herhalingen and $keer <= 1) {
     $keer = $keer + 1;
     //regelplaats($regel, $aantal_regels, $kolom);
     while ($regel < $aantal_regels) {
-        //echo $cel;
-        //echo "<br>";
-       // if ($cel < 100) {
             $kolom = 0;
             zoek($kolom, $aantal_kolommen, $einde, $woordzoeker, $regel, $arrayzoek, $uitgevoerd, $cel);
             $regel = $regel + 1;
-        //}
-        //else {echo 'Niet gevonden';}
     }
-    //$uitgevoerd = $uitgevoerd + 1;
 }
 
 $keer = 0;
 $zoek = strrev($zoek);
 $herhalingen = strlen($zoek);
 $arrayzoek = str_split($zoek);
+
 while ($uitgevoerd < $herhalingen and $keer <= 1) {
     $regel = 0;
     $keer = $keer + 1;
     //regelplaats($regel, $aantal_regels, $kolom);
     while ($regel < $aantal_regels) {
-        //echo $cel;
-        //echo "<br>";
-       // if ($cel < 100) {
             $kolom = 0;
             zoek($kolom, $aantal_kolommen, $einde, $woordzoeker, $regel, $arrayzoek, $uitgevoerd, $cel);
             $regel = $regel + 1;
-        //}
-        //else {echo 'Niet gevonden';}
     }
-    //$uitgevoerd = $uitgevoerd + 1;
 }
 
 
 echo $keuze;
+
+echo '<script type="text/javascript">';
+echo '$("#loading_spinner").hide()';
+echo '</script>';
 ?>
