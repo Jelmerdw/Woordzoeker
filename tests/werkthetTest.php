78a4bc0b0ea84ea6567a39e4f9b2a41596d26d15
unknown <147825@WKS-HP-1748.ASG.LOCAL>
2016-02-17 14:01:35 +0100

    komt ie dan

--- tests/werkthetTest.php
+++ tests/werkthetTest.php
@@ -1,14 +1,12 @@
 <?php
 
-include './zoeker_klik.php';
+include './verticaal.php';
 
 class StackTest extends PHPUnit_Framework_TestCase {
 
-    function test_zoek_horizontaal() {
-        $r = 0;
-        $a_r = 5;
-        $k = 0;
-        zoek_horizontaal($r, $a_r, $k);
-        $this->assertEquals(5, $r);
+    function test_onoff_verticaal() {
+        $v = 0;
+        onoff_verticaal ($v);
+        $this->assertEquals(1, $v);
     }
 }
--- verticaal.php
+++ verticaal.php
@@ -1,16 +1,24 @@
 <?php
-session_start();
-$verticaal = $_SESSION["verticaal"];
-if ($verticaal == 0){
-    $verticaal = 1;
-}
-else{
-    $verticaal = 0;
-}
-$_SESSION["verticaal"] = $verticaal;
 
-echo '<script type="text/javascript">';
-echo '$("#loading_spinner").hide()';
-echo '</script>';
+session_start();
+
+function onoff_verticaal(&$verticaal) {
+    //$verticaal = $_SESSION["verticaal"];
+    if ($verticaal == 0) {
+        $verticaal = 1;
+    } else {
+        $verticaal = 0;
+    }
+    $_SESSION["verticaal"] = $verticaal;
+
+    echo '<script type="text/javascript">';
+    echo '$("#loading_spinner").hide()';
+    echo '</script>';
+}
+
+if (isset($_SESSION['verticaal'])) {
+    $verticaal = $_SESSION["verticaal"];
+}
+onoff_verticaal($verticaal);
 ?>
 
