3f8647522d4a375a52618093d18f7b7287101edb
Stijn Wolters <148442@hpleerling.nl>
2016-02-22 16:10:13 +0100

    alstu

--- tests/werkthetTest.php
+++ tests/werkthetTest.php
@@ -11,35 +11,36 @@ class StackTest extends PHPUnit_Framework_TestCase {
         onoff_verticaal($v);
         $this->assertEquals(1, $v);
     }
-
     function test_onoff_horizontaal1() {
         $h = 0;
         onoff_verticaal($h);
         $this->assertEquals(1, $h);
     }
-
     function test_onoff_diagonaal1() {
         $d = 0;
         onoff_verticaal($d);
         $this->assertEquals(1, $d);
     }
-
     function test_onoff_verticaal2() {
         $v = 1;
         onoff_verticaal($v);
         $this->assertEquals(0, $v);
     }
-
     function test_onoff_horizontaal2() {
         $h = 1;
         onoff_verticaal($h);
         $this->assertEquals(0, $h);
     }
-
     function test_onoff_diagonaal2() {
         $d = 1;
         onoff_verticaal($d);
         $this->assertEquals(0, $d);
     }
-
+    
+    function test_uitvoeringen() {
+        $u = 0;
+        $h = 1;
+        onoff_verticaal($u, $h);
+        $this->assertEquals(1, $u);
+    }
 }
