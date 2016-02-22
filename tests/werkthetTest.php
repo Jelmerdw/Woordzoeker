2f247bcb0b848c3681472baf2768116679417694
Stijn Wolters <148442@hpleerling.nl>
2016-02-22 15:49:42 +0100

    unittest

--- tests/werkthetTest.php
+++ tests/werkthetTest.php
@@ -1,6 +1,8 @@
 <?php
 
 include './verticaal.php';
+include './horizontaal.php';
+include './diagonaal.php';
 
 class StackTest extends PHPUnit_Framework_TestCase {
 
@@ -8,5 +10,17 @@ class StackTest extends PHPUnit_Framework_TestCase {
         $v = 0;
         onoff_verticaal ($v);
         $this->assertEquals(1, $v);
+    }
+    
+    function test_onoff_horizontaal() {
+        $h = 0;
+        onoff_verticaal ($h);
+        $this->assertEquals(1, $h);
+    }
+    
+    function test_onoff_diagonaal() {
+        $d = 0;
+        onoff_verticaal ($d);
+        $this->assertEquals(1, $d);
     }
 }
